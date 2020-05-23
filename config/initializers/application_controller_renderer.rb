09a86e61d71df30254780ad9d73b4eaf46544505
mituya-hiroki <soccer.g.osaka.11@gmail.com>
2020-05-23 12:03:39 +0900

    finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+#以下を追記
+Refile.secret_key = 'a4584498426ee50e44619d8cd9ff10aa4e438471d299fbfed430e2e6811fff70b5576fc25c0ec1064f79d4ccd4f40ba8265f66eae41acddd6db3391f90b44d80'
